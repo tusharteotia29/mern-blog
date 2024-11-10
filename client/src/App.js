dbdc8c3c9e677710ff9c505f108f4200a8f2c4a1
tushar <teotiatushar29@gmail.com>
2024-11-10 15:31:10 +0530

    client commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import {Route, Routes} from "react-router-dom";
+import RegisterPage from './pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
+import CreatePost from './pages/CreatePost';
+import PostPage from './pages/PostPage';
+
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element={<LoginPage/>} />
+        <Route path={'/register'} element={<RegisterPage />} />
+        <Route path={"/create"} element={<CreatePost />} />
+        <Route path={"/post/:id"} element={<PostPage />} />
+      </Route>
+    </Routes>
+    </UserContextProvider>
+    
+   
+  );
+}
+
+export default App;
